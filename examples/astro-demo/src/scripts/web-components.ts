52d4c3afbdb19767ef9205ddf83160d782b4e9c5
andersseen <andriipap01@gmail.com>
2026-06-24 20:36:24 +0200

    feat: update web components

--- examples/astro-demo/src/scripts/web-components.ts
+++ examples/astro-demo/src/scripts/web-components.ts
@@ -1,3 +1,15 @@
-import { defineCustomElements } from "@andersseen/web-components/loader";
+import { defineCustomElement as defineAndNavbar } from "@andersseen/web-components/components/and-navbar.js";
+import { defineCustomElement as defineAndBadge } from "@andersseen/web-components/components/and-badge.js";
+import { defineCustomElement as defineAndButton } from "@andersseen/web-components/components/and-button.js";
+import { defineCustomElement as defineAndCard } from "@andersseen/web-components/components/and-card.js";
+import { defineCustomElement as defineAndCardContent } from "@andersseen/web-components/components/and-card-content.js";
+import { defineCustomElement as defineAndCardHeader } from "@andersseen/web-components/components/and-card-header.js";
+import { defineCustomElement as defineAndCardTitle } from "@andersseen/web-components/components/and-card-title.js";
 
-defineCustomElements(window);
+defineAndNavbar();
+defineAndBadge();
+defineAndButton();
+defineAndCard();
+defineAndCardContent();
+defineAndCardHeader();
+defineAndCardTitle();
